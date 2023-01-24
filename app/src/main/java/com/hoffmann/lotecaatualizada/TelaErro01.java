5d206977dfbb533b9006854ab7c3387964f9810c
carlos.hoffmann <carlos.hoffmann@foursys.com.br>
2023-01-24 20:57:03 -0300

    Melhorias na loteca

---
+++ app/src/main/java/com/hoffmann/lotecaatualizada/TelaErro01.java
@@ -0,0 +1,14 @@
+package com.hoffmann.lotecaatualizada;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaErro01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_erro01);
+    }
+}
